fc454215810962dcf630a1e08a13cc366f741c11
Cole Spears <cole.spears1110@gmail.com>
2019-05-18 18:27:58 -0500

    added v1 rect

--- p5/stars-project/sketch.js
+++ p5/stars-project/sketch.js
@@ -1,7 +1,16 @@
-let json
+// TODO: Boundaries for rect. Don't let go offscreen
+// TODO: Don't default transform to center. Mouse can go anywhere inside rect
+// TODO: rect transorm. Width, Height, Rotate
+
+let json;
 let stars = [];
 //magnitude_cutoff is low cutoff; used to remove least brightest stars.
 let magnitude_cutoff;
+
+let rect_window;
+let rect_center, rect_width, rect_height;
+
+let moveRect = false;
 
 function preload(){
   if (magnitude_cutoff == null) {
@@ -15,30 +24,103 @@ function setup() {
   // 0 <= y <= 599
   // -0.9923 <= z <= 19.629
   createCanvas(850, 600);
+
+  rectMode(CENTER);
+  rect_center = createVector(width/2, height/2);
+  rect_width = 400;
+  rect_height = 25;
 }
 
 function draw() {
   background(7,11,52);
 
+  //Draw Stars
   for (let star of stars) {
     //AbsMag is set to star.z
-
-    //second arg of map, maybe magnitude_cutoff?
-    let alpha = map(star.z, -0.9923, 19.629, 0, 255); // max alpha? 150 is nice, but a too dim?
-
-    stroke(255, 255, 255, alpha)
+    let alpha = map(star.z, magnitude_cutoff, 19.629, 0, 255); // max alpha? 150 is nice, but a too dim?
+    stroke(255, 255, 255, alpha);
+    strokeWeight(1);
     point(star.x, star.y);
+  }
+
+  //Draw Rect
+  stroke(255,0,0);
+  strokeWeight(1);
+  noFill();
+  rect_window = rect(rect_center.x, rect_center.y, rect_width, rect_height);
+
+  //Will use to see if mouse or star is in rect
+  if (calculateIsInsideRect(width/2, height/2)){
+    //console.log("Yes");
+  }
+}
+
+function mousePressed() {
+  if (calculateIsInsideRect(mouseX, mouseY)){
+    moveRect = true;
+  }
+}
+
+function mouseDragged() {
+  if (moveRect) {
+    rect_center.set(mouseX, mouseY);
+  }
+}
+
+function mouseReleased(){
+  if (moveRect) {
+    moveRect = false;
   }
 }
 
 function trimJSON() {
-  //trim JSON based on magnitude_cutoff, place in stars array
-  //AbsMag is set to Vector.z
+  // trim JSON based on magnitude_cutoff, place in stars array
+  // AbsMag is set to Vector.z
   for(let i = 0; i < Object.keys(json).length; i++) {
     if (json[i].AbsMag >= magnitude_cutoff) {
       stars.push(createVector(json[i].X, json[i].Y, json[i].AbsMag))
     }
   }
+}
+
+function calculateIsInsideRect(x, y) {
+  // Create two triangles in rect, test areas. Will help with rotated rect
+
+  //  A(X,Y)--------B(X,Y)
+  //     |   P(X,Y)   |
+  //  C(X,Y)--------D(X,Y)
+
+  // if (ABC == APB + BPC + APC || BCD == BPC + CPD + BPD){
+  //  is inside
+  //}
+
+  let A = createVector(rect_center.x - rect_width/2, rect_center.y - rect_height/2)
+  let B = createVector(rect_center.x + rect_width/2, rect_center.y - rect_height/2)
+  let C = createVector(rect_center.x - rect_width/2, rect_center.y + rect_height/2)
+  let D = createVector(rect_center.x + rect_width/2, rect_center.y + rect_height/2)
+
+  let P = createVector(x, y);
+
+  let ABC = calculateAreaTriangle(A, B, C);
+  let BCD = calculateAreaTriangle(B, C, D);
+
+  let APB = calculateAreaTriangle(A, P, B);
+  let BPC = calculateAreaTriangle(B, P, C);
+  let APC = calculateAreaTriangle(A, P, C);
+
+  // Already calculated BPC
+  let CPD = calculateAreaTriangle(C, P, D);
+  let BPD = calculateAreaTriangle(B, P, D);
+
+  if (ABC == APB + BPC + APC || BCD == BPC + CPD + BPD){
+    return true;
+  }
+  return false;
+}
+
+function calculateAreaTriangle(v1, v2, v3) {
+  let area = (v1.x*(v2.y - v3.y) + v2.x*(v3.y - v1.y) + v3.x*(v1.y - v2.y))/2
+  return Math.abs(area);
 }
 
 // start server:
@@ -47,3 +129,6 @@ function trimJSON() {
 // star data from:
 // https://starmap2d.appspot.com/
 // https://starmap2d.appspot.com/download/stars.csv
+
+// https://github.com/astronexus/HYG-Database
+// comes with x,y,z
